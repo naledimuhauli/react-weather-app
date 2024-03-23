b12e092dcb9c4e9926bdc59760fe5b2678e0a691
naledimuhauli <naledimuhauli@gmail.com>
2024-03-23 03:22:07 +0200

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -9,11 +9,13 @@ export default function Weather(){
                     <div className="col-9">
                 <input type="text"
                 placeholder="Enter a city"
-                className="form-control" />
+                className="form-control"
+                autoFocus="on" />
                 </div>
                 <div className="col-3">
                 <input type="submit"
-                value="Search" />
+                value="Search"
+                className="btn btn-primary" />
                 </div>
                 </div>
             </form>
@@ -22,11 +24,18 @@ export default function Weather(){
             <li>Friday 20:59</li>
             <li>Clear sky</li>
            </ul>
-           <div className="row">
+           <div className="row mt-3">
             <div className="col-6">
+            <div className="clearfix">
            <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-             alt="Clear sky" /> 5°C
+             alt="Clear sky"
+             className="float-left"
+              /> 
+             <span className="temperature">10 </span>
+             <span className="unit">°C</span>
              </div>
+             </div>
+
              <div className="col-6">
                 <ul>
                     <li>
